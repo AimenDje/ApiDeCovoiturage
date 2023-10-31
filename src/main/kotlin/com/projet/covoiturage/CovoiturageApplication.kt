14e3003687d0ad9eb6c041324b27a7d5fcb8596a
Mousseau Langevin Gabriel <1846508@crosemont.qc.ca>
2023-10-31 09:03:54 -0400

    création projet spring kotlin

---
+++ src/main/kotlin/com/projet/covoiturage/CovoiturageApplication.kt
@@ -0,0 +1,11 @@
+package com.projet.covoiturage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CovoiturageApplication
+
+fun main(args: Array<String>) {
+	runApplication<CovoiturageApplication>(*args)
+}
