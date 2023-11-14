8385b9be3f028e4542e80ce27812e3d00b6556aa
Gabriel Mousseau Langevin <1846508@crosemont.qc.ca>
2023-11-14 12:13:25 -0500

    Dépendances sécurité en commentaire pour l'instant

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,10 @@ dependencies {
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	//implementation("org.springframework.boot:spring-boot-devtools:3.1.5")
 
-	implementation("org.springframework.boot:spring-boot-starter-security")
+/*	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.security:spring-security-oauth2-resource-server")
 	implementation("org.springframework.security:spring-security-oauth2-jose")
-	implementation("org.springframework.security:spring-security-config")
+	implementation("org.springframework.security:spring-security-config")*/
 }
 
 tasks.withType<KotlinCompile> {
