801cf870c9692a4bb35510fec19c5f19130b7964
Aimen <djeaimen@gmail.com>
2023-11-17 19:23:30 -0500

    implémentation du Service pour le trajet

--- src/main/kotlin/com/projet/covoiturage/Service/TrajetService.kt
+++ src/main/kotlin/com/projet/covoiturage/Service/TrajetService.kt
@@ -3,10 +3,13 @@ package com.projet.covoiturage.Service
 import com.projet.covoiturage.DAO.TrajetDAO
 import com.projet.covoiturage.Model.Reservation
 import com.projet.covoiturage.Model.Trajet
+import com.projet.covoiturage.Model.Utilisateur
 import org.springframework.stereotype.Service
 
 @Service
 class TrajetService (val dao: TrajetDAO){
+    fun chercherTrajetsParUtilisateur(idUtilisateur: Int) = dao.chercherTrajetsParutilisateur(idUtilisateur)
+    fun chercherTrajetParUtilisateur(idTrajet: Int, idUtilisateur: Int) = dao.chercherTrajetParUtilisateur(idTrajet, idUtilisateur)
     fun chercherTous() = dao.chercherTous()
 
     fun chercherParId(id: Int) = dao.chercherParId(id)
